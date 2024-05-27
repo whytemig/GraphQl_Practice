24b37930f58e9acbe07afb470ab3f793f89a1007
miguel whyte <whytemig@gmail.com>
2024-05-27 11:38:28 -0400

    client side

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -2,8 +2,45 @@ import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import { Layout, QueryResult } from "../components";
 import { useParams } from "react-router-dom";
+import TrackDetail from "../components/track-detail";
+
+const GET_TRACK = gql`
+  query GetTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      thumbnail
+      length
+      moduleCount
+      description
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
 
 export const Track = () => {
   const { trackId } = useParams();
-  return <Layout></Layout>;
+  const { loading, error, data } = useQuery(GET_TRACK, {
+    variables: {
+      trackId,
+    },
+  });
+
+  return (
+    <Layout>
+      <QueryResult error={error} loading={loading} data={data}>
+        <TrackDetail track={data?.track} />
+      </QueryResult>
+    </Layout>
+  );
 };
